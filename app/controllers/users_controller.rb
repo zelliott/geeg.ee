65e931d087cfa6398c2cb20418372c29dcb066be
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-01-20 01:36:52 +0530

    Adds error messages for user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,26 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      redirect_to @user
+    else
+      flash[:errors] = @user.errors
+      redirect_to signup_path
     end
-    
-    def new
-        @user = User.new
-    end
-    
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            sign_in @user
-            redirect_to @user
-        else
-            render 'new'
-        end
-    end
+  end
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation, :alias)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :alias)
+  end
 end
