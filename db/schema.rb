e7f485ecec7ee6cbd1d64910e2f91580869cde5b
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-01-25 18:15:33 +0530

    Adds password reset fields to user.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140114165850) do
+ActiveRecord::Schema.define(version: 20140125124340) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140114165850) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.string   "alias"
+    t.string   "password_reset_token"
+    t.datetime "password_expires_after"
   end
 
   add_index "users", ["remember_token"], name: "index_users_on_remember_token"
