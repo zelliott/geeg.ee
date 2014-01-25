470701fe1a7b0c67363606a5d1f817b945ee4cdd
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-01-26 03:27:03 +0530

    Completes update action for resetting password.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -11,4 +11,15 @@ class PasswordResetsController < ApplicationController
   def edit
     @user = User.find_by_password_reset_token!(params[:id])
   end
+  
+  def update
+    @user = User.find_by_password_reset_token!(params[:id])
+    if @user.password_expires_after > Time.now
+      redirect_to new_password_reset_path, :alert => "Reset token has expired. Please send a new reset request."
+    elsif @user.updateattributes(params[:user])
+      redirect_to root_url, :notice => "Your password has been successfully reset."
+    else
+      render :edit
+    end
+  end
 end
