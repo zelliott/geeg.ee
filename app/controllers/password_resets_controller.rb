815113f76cc6e77d798326b7d0a57eb9bcd4b67e
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-01-26 04:20:19 +0530

    Permits password to be modified.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,9 +14,10 @@ class PasswordResetsController < ApplicationController
   
   def update
     @user = User.find_by_password_reset_token!(params[:id])
+    @user.update_attributes!(params[:user].permit(:password, :password_confirmation))
     if @user.password_expires_after > Time.now
       redirect_to new_password_reset_path, :alert => "Reset token has expired. Please send a new reset request."
-    elsif @user.updateattributes(params[:user])
+    elsif @user.update_attributes(params[:user].permit(:password, :password_confirmation))
       redirect_to root_url, :notice => "Your password has been successfully reset."
     else
       render :edit
