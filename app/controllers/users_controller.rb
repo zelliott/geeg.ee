d37a831a45fb990023635f1c84362547ac7a34b6
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-02-12 02:59:08 +0530

    Fixes CookieOverflow error when creating users - Stores only full error messages in flash.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,12 @@ class UsersController < ApplicationController
   end
   
   def create
-    # Cookies::CookieOverflow
     @user = User.new(user_params)
     if @user.save
       sign_in(@user, 1)
       redirect_to @user
     else
-      flash[:errors] = @user.errors
+      flash[:errors] = @user.errors.full_messages
       redirect_to signup_path
     end
   end
