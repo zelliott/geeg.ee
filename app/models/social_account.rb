dc31be1396b294117a8beaa206c37631e7c0ea51
Gaurav Jhaveri <jhaveri.gaurav@gmail.com>
2014-02-25 18:42:01 +0530

    Changes empty strings to nil before saving social links.

--- app/models/social_account.rb
+++ app/models/social_account.rb
@@ -1,3 +1,11 @@
 class SocialAccount < ActiveRecord::Base
   belongs_to :user
+  before_save :normalize_blank_values
+
+  def normalize_blank_values
+    attributes.each do |column, value|
+      self[column].present? || self[column] = nil
+    end
+  end
+  
 end
